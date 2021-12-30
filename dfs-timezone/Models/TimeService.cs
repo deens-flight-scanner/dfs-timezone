46b79a86cd18c86f7627b8c4365bdb2665a57b37
deenr <deenrayman@outlook.be>
2021-12-30 04:25:22 +0100

    time difference change

--- dfs-timezone/Models/TimeService.cs
+++ dfs-timezone/Models/TimeService.cs
@@ -16,7 +16,7 @@ namespace Models
             DateTime startTime = new DateTime(dateNow.Year, dateNow.Month, dateNow.Day, timeDeparture.Hour, timeDeparture.Minutes, timeDeparture.Seconds);
             DateTime endTime = new DateTime(dateNow.Year, dateNow.Month, dateNow.Day, timeArrival.Hour, timeArrival.Minutes, timeArrival.Seconds);
 
-            TimeSpan duration = startTime - endTime;
+            TimeSpan duration = endTime.Subtract(startTime);
 
             return duration.TotalMinutes;
         }
